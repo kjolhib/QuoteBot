6580a640538d7fd07ce1b34af38c1a5e40548263
kjolhib <alanren972@gmail.com>
2026-04-21 22:43:15 +1000

    added tests, batch 1

---
+++ tests/dnd/test_session.py
@@ -0,0 +1,71 @@
+import pytest
+
+from classes.dnd_session import DnDSession
+from exceptions.dice import (
+  die_alr_exists_error,
+  invalid_faces_error,
+  no_dice_in_sesh_error,
+  too_many_dice_error
+)
+
+"""
+session: the current DnDSession class
+Testing various commands relating to sessions
+"""
+
+@pytest.fixture
+def session():
+  return DnDSession()
+
+@pytest.fixture
+def session_with_die(session: DnDSession):
+  session.create_new_dice(6, "a")
+  return session
+
+"""
+Init tests
+"""
+def test_initial_dice_empty(session: DnDSession):
+  assert not session.current_session_dice
+
+def test_initial_inactive(session: DnDSession):
+  assert not session.is_active
+
+def test_initial_start_time(session: DnDSession):
+  assert session.start_time == 0
+
+"""
+Dice related tests
+"""
+def test_new_die_valid(session_with_die: DnDSession):
+  assert session_with_die.current_session_dice
+  assert len(session_with_die.current_session_dice) == 1
+
+def test_remove_die_valid(session_with_die: DnDSession):
+  assert session_with_die.current_session_dice
+  assert len(session_with_die.current_session_dice) == 1
+
+  session_with_die.remove_dice("a")
+  assert not session_with_die.current_session_dice
+
+@pytest.mark.parametrize("die_num", [1, 2, 3, 5])
+def test_new_die__num_invalid(session: DnDSession, die_num: int):
+  with pytest.raises(invalid_faces_error.InvalidFacesError):
+    session.create_new_dice(die_num, "a")
+
+def test_new_die__alr_exists(session_with_die: DnDSession):
+  with pytest.raises(die_alr_exists_error.DieAlrExistsError):
+    session_with_die.create_new_dice(6, "a")
+
+def test_remove_die__no_scenario(session_with_die: DnDSession):
+  with pytest.raises(no_dice_in_sesh_error.NoDiceInSeshError):
+    session_with_die.remove_dice("b")
+
+def test_add_die__too_many(session: DnDSession):
+  for i in range(100):
+    session.create_new_dice(7, f"{i}")
+
+  assert len(session.current_session_dice) == 100
+
+  with pytest.raises(too_many_dice_error.TooManyDiceError):
+    session.create_new_dice(7, "101")
---
+++ tests/utility/test_dice.py
@@ -0,0 +1,38 @@
+import pytest
+from helpers.DiceHelpers import compute_roll
+
+from exceptions.dice import invalid_faces_error
+
+"""
+die_num: number of faces
+Testing valid and invalid number of faces.
+"""
+@pytest.mark.parametrize("die_num", [1, 2, 3, 5, -2])
+def test_roll_invalid_faces(die_num: int):
+  with pytest.raises(invalid_faces_error.InvalidFacesError):
+    compute_roll(die_num)
+
+@pytest.mark.parametrize("die_num", [4, 6, 100])
+def test_roll_valid_faces(die_num: int):
+  result = compute_roll(die_num)
+  assert f"D{die_num}" in result
+
+"""
+addon: adds onto the result
+Tests positive, negative, and 0
+"""
+
+def test_positive_addon():
+  result = compute_roll(6, addon=2)
+  assert f"+" in result
+  assert "2" in result
+
+def test_negative_addon():
+  result = compute_roll(6, addon=-2)
+  assert f"-" in result
+  assert "2" in result
+
+def test_0_addon():
+  result = compute_roll(6)
+  assert "-" not in result
+  assert "+" not in result
